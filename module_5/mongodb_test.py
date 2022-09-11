52edf70e59894d9dccd8a58d17333b668190c54b
estilicht <95283751+estilicht@users.noreply.github.com>
2022-09-11 19:33:20 +0300

    Update mongodb_test.py

--- module_5/mongodb_test.py
+++ module_5/mongodb_test.py
@@ -1,1 +1,9 @@
+import pymongo
+from pymongo import MongoClient
 
+url="mongodb://estlichtenstein:Chana12!@ac-cgmlvgz-shard-00-00.hypg9mr.mongodb.net:27017,ac-cgmlvgz-shard-00-01.hypg9mr.mongodb.net:27017,ac-cgmlvgz-shard-00-02.hypg9mr.mongodb.net:27017/?ssl=true&replicaSet=atlas-etuuci-shard-0&authSource=admin&retryWrites=true&w=majority"
+client = MongoClient(url)
+db = client.pytech
+print (db.list_collection_names)  
+
+input('Press ENTER to exit')
