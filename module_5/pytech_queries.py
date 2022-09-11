76fd90fce6898d7d7a618bc3348c5a735473a289
estilicht <95283751+estilicht@users.noreply.github.com>
2022-09-12 00:17:14 +0300

    Update pytech_queries.py

--- module_5/pytech_queries.py
+++ module_5/pytech_queries.py
@@ -1,1 +1,29 @@
+import pymongo
+from pymongo import MongoClient
 
+
+
+url="mongodb+srv://estlichtenstein:Chana12!@cluster0.hypg9mr.mongodb.net/pytech"
+client = MongoClient(url)
+db = client.pytech
+students = db.students
+
+
+docs = students.find({})
+print(" -- DISPLAYING STUDENTS DOCUMENTS FROM find() QUERY -- ")
+
+
+for doc in docs:
+    print("Student ID: " + str(doc["student_id"]))
+    print("First Name: " + str(doc["first_name"]))
+    print("Last Name: " + str(doc["last_name"]))
+    print()
+
+print()
+print(" -- DISPLAYING STUDENTS DOCUMENTS FROM find_one() QUERY -- ")
+
+doc = students.find_one({"student_id": "1007"})
+ 
+print("Student ID: " + str(doc["student_id"]))
+print("First Name: " + str(doc["first_name"]))
+print("Last Name: " + str(doc["last_name"]))
