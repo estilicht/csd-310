8f3f2568112c09e5f0834df4450c844f4a7ded68
estilicht <95283751+estilicht@users.noreply.github.com>
2022-09-11 23:28:16 +0300

    Update pytech_insert.py

--- module_5/pytech_insert.py
+++ module_5/pytech_insert.py
@@ -1,1 +1,38 @@
+import pymongo
+from pymongo import MongoClient
 
+url="mongodb+srv://estlichtenstein:Chana12!@cluster0.hypg9mr.mongodb.net/pytech"
+client = MongoClient(url)
+db = client.pytech
+pytech = db["PyTech"]
+
+records = [
+    {
+        "student_id": "1007",
+        "first_name": "Fred",
+        "last_name": "Fuller"
+    },
+    {
+        "student_id": "1008",
+        "first_name": "Mary",
+        "last_name": "Miller"
+    },
+    {
+        "student_id": "1009",
+        "first_name": "Sam",
+        "last_name": "Bella"
+    }
+]
+
+for record in records:
+    new_student_Id = pytech.insert_one(record).inserted_id
+    print(new_student_Id)
+
+docs = pytech.find()
+
+for doc in docs:
+    print(doc)
+
+print(pytech.find_one({"student_id": "1008"}))
+
+input('Press ENTER to exit')
